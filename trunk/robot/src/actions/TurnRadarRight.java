0f534ea643f5e567f33a866a066da729888a4d36
Gibbo87 <Gibbo87@3a219481-810d-483a-9e49-147a710d9614>
2010-09-30 15:49:40 +0000

    First Commit

---
+++ trunk/robot/src/actions/TurnRadarRight.java
@@ -0,0 +1,18 @@
+package actions;
+
+public class TurnRadarRight {
+	double degree;
+
+	public double getDegree() {
+		return degree;
+	}
+
+	public void setDegree(double degree) {
+		this.degree = degree;
+	}
+
+	public TurnRadarRight(double degree) {
+		super();
+		this.degree = degree;
+	}
+}
